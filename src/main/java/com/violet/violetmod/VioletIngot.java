175d8211ff7b9ed1d924eebf8b3eb299c96612bf
RoBoLilly <ultrahoodsense@gmail.com>
2015-07-08 19:17:25 -0600

    Fixed src
    Made src a shortcut

--- src/main/java/com/violet/violetmod/VioletIngot.java
+++
@@ -1,9 +0,0 @@
-package com.violet.violetmod;
-
-import net.minecraft.item.Item;
-
-public class VioletIngot extends Item{
-	protected VioletIngot(){
-		this.setCreativeTab(VioletMod.tabVioletMod);
-	}
-}
